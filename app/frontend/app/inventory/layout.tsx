c4c0d1cc95bbaa3dfea79f1a4d47792dc4f17a14
yokenzan <31175068+yokenzan@users.noreply.github.com>
2024-08-14 01:44:56 +0900

    apply layout and css for page

---
+++ app/frontend/app/inventory/layout.tsx
@@ -0,0 +1,21 @@
+'use client';
+import styles from './styles.module.css';
+
+export default function InventoryLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+        <div className={styles.layout}>
+            <header className={styles.header}>ヘッダー</header>
+            <div className={styles.container}>
+                <aside className={styles.navbar}>サイドバー</aside>
+                <main className={styles.content}>
+                    <section>{children}</section>
+                </main>
+            </div>
+            <footer className={styles.footer}>フッター</footer>
+        </div>
+    );
+}
